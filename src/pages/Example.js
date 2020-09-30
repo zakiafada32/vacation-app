c29ecdc0f421311cfefb6bf29e317884087138da
Zaki Afada <afadazaki32@gmail.com>
2020-09-30 21:23:56 +0700

    breadcrumb component

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
-import InputNumber from 'elements/Form/InputNumber';
-import InputDate from 'elements/Form/InputDate';
+// import InputNumber from 'elements/Form/InputNumber';
+// import InputDate from 'elements/Form/InputDate';
+import Breadcrumb from 'elements/Breadcrumb';
 
-export default class Example extends Component {
-  // example input number
-  // state = {
-  //   value: '1',
-  // };
-  // handleChange = (e) => {
-  //   this.setState({ [e.target.name]: e.target.value });
-  // };
-  // render() {
-  //   console.log(this.state);
-  //   return (
-  //     <div className='container'>
-  //       <div
-  //         className='row align-items-center justify-content-center'
-  //         style={{ height: '100vh' }}
-  //       >
-  //         <div className='col-auto'>
-  //           <InputNumber
-  //             max={30}
-  //             suffix=' night'
-  //             onChange={this.handleChange}
-  //             name='value'
-  //             value={this.state.value}
-  //           />
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+// example input number
+/* export default class Example extends Component {
+  state = {
+    value: '1',
+  };
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div className='container'>
+        <div
+          className='row align-items-center justify-content-center'
+          style={{ height: '100vh' }}
+        >
+          <div className='col-auto'>
+            <InputNumber
+              max={30}
+              suffix=' night'
+              onChange={this.handleChange}
+              name='value'
+              value={this.state.value}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+ }
+*/
 
-  // example input date
+// example input date
+/* export default class Example extends Component {
   state = {
     value: {
       startDate: new Date(),
@@ -57,6 +61,30 @@ export default class Example extends Component {
               name='value'
               value={this.state.value}
             />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+*/
+
+// example breadcrumb
+export default class Example extends Component {
+  render() {
+    const breadcrumb = [
+      { pageTitle: 'Home', pageHref: '' },
+      { pageTitle: 'House Details', pageHref: '' },
+    ];
+
+    return (
+      <div className='container'>
+        <div
+          className='row align-item-center justify-content-center'
+          style={{ height: '100vh' }}
+        >
+          <div className='col-auto'>
+            <Breadcrumb data={breadcrumb} />
           </div>
         </div>
       </div>
